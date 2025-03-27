cb6d3402bc0191b7fc39ced229e9d03346d599db
Fryteusz <164088712+Fryteusz@users.noreply.github.com>
2025-03-27 12:27:06 +0100

    Rotate

--- Lab4/Lab4/Form1.Designer.cs
+++ Lab4/Lab4/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,30 +42,79 @@
             this.pictureBox1.Location = new System.Drawing.Point(210, 37);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(221, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(210, 304);
+            this.button1.Location = new System.Drawing.Point(210, 246);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 275);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Rotate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "90°";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "180°";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 69);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 20);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "270°";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +122,10 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- Lab4/Lab4/Form1.cs
+++ Lab4/Lab4/Form1.cs
@@ -16,10 +16,52 @@ namespace Lab4
         {
             InitializeComponent();
         }
+        private Bitmap RotateImage(Bitmap oryginalnyObraz, float kąt)
+        {
+            double radians = Math.Abs(kąt * Math.PI / 180.0);
+            int szerokosc = (int)(Math.Abs(oryginalnyObraz.Width * Math.Cos(radians)) + Math.Abs(oryginalnyObraz.Height * Math.Sin(radians)));
+            int wysokosc = (int)(Math.Abs(oryginalnyObraz.Height * Math.Cos(radians)) + Math.Abs(oryginalnyObraz.Width * Math.Sin(radians)));
 
+            Bitmap obroconyObraz = new Bitmap(szerokosc, wysokosc);
+
+            using (Graphics g = Graphics.FromImage(obroconyObraz))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                g.TranslateTransform(szerokosc / 2, wysokosc / 2);
+                g.RotateTransform(kąt);
+                g.DrawImage(oryginalnyObraz, -oryginalnyObraz.Width / 2, -oryginalnyObraz.Height / 2);
+            }
+
+            return obroconyObraz;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = Image.FromFile("C:\\Users\\Fryderyk\\Documents\\GitHub\\ProgramowanieWizualne\\Lab4\\image1.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                    float kąt = 0;
+                    if (radioButton1.Checked)
+                    {
+                        kąt = 90;
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        kąt = 180;
+                    }
+                    else if (radioButton3.Checked)
+                    {
+                        kąt = 270;
+                    }
+                    Bitmap oryginalnyObraz = new Bitmap(pictureBox1.Image); 
+                    Bitmap obroconyObraz = RotateImage(oryginalnyObraz, kąt);
+                    pictureBox1.Image = obroconyObraz;
+            }
         }
     }
 }
