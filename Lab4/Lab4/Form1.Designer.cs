028d97f031dcc764f9e9fc2ea41bb2766a32f835
Fryteusz <164088712+Fryteusz@users.noreply.github.com>
2025-03-27 13:00:53 +0100

    OnlyGreen

--- Lab4/Lab4/Form1.Designer.cs
+++ Lab4/Lab4/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,6 +39,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(210, 37);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(221, 176);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -51,17 +53,29 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "OnlyGreen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +83,7 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Lab4/Lab4/Form1.cs
+++ Lab4/Lab4/Form1.cs
@@ -16,10 +16,49 @@ namespace Lab4
         {
             InitializeComponent();
         }
+        private Bitmap TylkoZielony(Bitmap oryginal)
+        {
+            Bitmap tylkoZielony = new Bitmap(oryginal.Width, oryginal.Height, oryginal.PixelFormat);
+
+            for (int x = 0; x < oryginal.Width; x++)
+            {
+                for (int y = 0; y < oryginal.Height; y++)
+                {
+                    Color kolor = oryginal.GetPixel(x, y);
+                    int prógZieleni = 100;
+                    int prógCzerwieniINiebieskiego = 100;
+                    if (kolor.G >= prógZieleni && kolor.R <= prógCzerwieniINiebieskiego && kolor.B <= prógCzerwieniINiebieskiego)
+                    {
+                        tylkoZielony.SetPixel(x, y, kolor);
+                    }
+                    else
+                    {
+                        tylkoZielony.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+            return tylkoZielony;
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             pictureBox1.Image = Image.FromFile("C:\\Users\\Fryderyk\\Documents\\GitHub\\ProgramowanieWizualne\\Lab4\\image1.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                {
+                    Bitmap oryginalnyObraz = new Bitmap(pictureBox1.Image);
+                    Bitmap tylkoZielonyObraz = TylkoZielony(oryginalnyObraz);
+                    pictureBox1.Image = tylkoZielonyObraz;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Najpierw wczytaj obrazek.");
+            }
         }
     }
 }
